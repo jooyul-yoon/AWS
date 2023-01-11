9234153a64818fc465535b8147bdbf1d2f9171f5
Jooyul Yoon <juyeolyoon@gmail.com>
2023-01-11 14:55:17 +0000

    Project Created

---
+++ src/main/java/worky/worky/WorkyApplication.java
@@ -0,0 +1,13 @@
+package worky.worky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/worky/worky/WorkyApplicationTests.java
@@ -0,0 +1,13 @@
+package worky.worky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
